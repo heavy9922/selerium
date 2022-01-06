bf493939173a830dc2a2da8ca711574e841405f9
yefersonCastiblancoNivelics <93614180+yefersonCastiblancoNivelics@users.noreply.github.com>
2022-01-06 13:25:29 -0500

    primer ejercicio

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+import time
+
 
 def first_exercise():
     a  = 0 
@@ -7,11 +10,23 @@ def first_exercise():
         list.append(a)
     print(list)
 
+def secundary_exercise(url):
+    #Chromedriver
+    ruta_chromedriver = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    driver = webdriver.Chrome(executable_path=ruta_chromedriver)
 
-
+    driver.maximize_window()
+    driver.get(url)
+    # time.sleep(5)
+    # driver.find_element_by_class_name('vbplogin').click()
+    # driver.find_element_by_name('log').send_keys('hola')
+    # time.sleep(10)
+    # driver.find_element_by_name('pwd').send_keys('secreto')
+    time.sleep(10)
 
 def main():
     first_exercise()
+    secundary_exercise('https://www.python.org/')
 
 if __name__ == '__main__':
     main()
