38a0954af2d73d15bf1c28a8a3f5d3be260720f4
yefersonCastiblancoNivelics <93614180+yefersonCastiblancoNivelics@users.noreply.github.com>
2022-01-06 13:23:21 -0500

    primer ejercicio

---
+++ src/main.py
@@ -0,0 +1,17 @@
+
+def first_exercise():
+    a  = 0 
+    list = []
+    while a < 15:
+        a+=1
+        list.append(a)
+    print(list)
+
+
+
+
+def main():
+    first_exercise()
+
+if __name__ == '__main__':
+    main()
